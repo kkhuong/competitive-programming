817b64b082cc905f7c266677f7eb9fa33f972101
Kevin Khuong <kkhuong@ucsd.edu>
2018-08-15 08:52:08 -0700

    Testing std::lower_bound and std::upper_bound

---
+++ experimenting/upper_bound_stl.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main() {
+    std::vector<int> xs = {10,20,30,30,20,10,10,20};
+    std::sort(xs.begin(), xs.end());
+
+    auto l33 = std::lower_bound(xs.cbegin(), xs.cend(), 33);
+    auto l20 = std::lower_bound(xs.cbegin(), xs.cend(), 20);
+    auto l30 = std::lower_bound(xs.cbegin(), xs.cend(), 30);
+    auto u33 = std::upper_bound(xs.cbegin(), xs.cend(), 33);
+    auto u20 = std::upper_bound(xs.cbegin(), xs.cend(), 20);
+    auto u30 = std::upper_bound(xs.cbegin(), xs.cend(), 30);
+
+    std::cout << (xs.end() == l33) << std::endl;
+    std::cout << (xs.end() != l20) << std::endl;
+    std::cout << (xs.end() != l30) << std::endl;
+    std::cout << (xs.end() == u33) << std::endl;
+    std::cout << (xs.end() != u20) << std::endl;
+    std::cout << (xs.end() == u30) << std::endl;
+}
