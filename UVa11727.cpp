2b30874398959d906a77454feb52f5c70789682e
Kevin Khuong <kkhuong@ucsd.edu>
2018-08-15 07:03:08 -0700

    Experimented with STL array instead of vector

--- UVa11727.cpp
+++ UVa11727.cpp
@@ -1,16 +1,16 @@
 #include <cstdio>
-#include <vector>
+#include <array>
 #include <algorithm>
 using namespace std;
 
 int main() {
     int N, a, b, c, C=0;
-    vector<int> xs;
+    array<int, 3> xs;
 
     scanf("%d\n", &N);
     while (N--) {
         scanf("%d %d %d\n", &a, &b, &c);
-        xs = {a,b,c};
+        xs[0]=a,xs[1]=b,xs[2]=c;
         sort(xs.begin(), xs.end());
         printf("Case %d: %d\n", ++C, xs[1]);
     }
